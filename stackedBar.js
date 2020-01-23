88f1b3b9f0116e64719db1565f76581857733ce9
Daniel Bergey <bergey@simspace.com>
2020-01-23 17:33:32 -0500

    horizontal bars so text is easier to read

--- stackedBar.js
+++ stackedBar.js
@@ -1,6 +1,6 @@
 // Setup svg using Bostock's margin convention
 
-var margin = {top: 20, right: 160, bottom: 250, left: 80};
+var margin = {top: 20, right: 160, bottom: 80, left: 250};
 
 var width = 1200 - margin.left - margin.right,
     height = 800 - margin.top - margin.bottom;
@@ -55,14 +55,14 @@ function pivotAndStack( csvData ) {
                            return d;
                        })
                        .sort( (a, b) => b.total - a.total ) // descending
-                       .slice(0, 500);
+                       .slice(1, 500);
 
     let stack = d3.stack() .keys(phases);
     let dataset = stack(slowest)
         .map( series => series.map( d => ({
-            x: d.data.name,
-            y0: d[0],
-            y: d[1],
+            name: d.data.name,
+            start_ms: d[0],
+            end_ms: d[1],
             data: d.data
         })));
     console.log(dataset);
@@ -72,14 +72,14 @@ function pivotAndStack( csvData ) {
 function render([phases, dataset]) {
 
     // Set x, y and colors
-    var x = d3.scaleBand()
-            .domain(dataset[0].map(function(d) { return d.x; }))
+    var y = d3.scaleBand()
+            .domain(dataset[0].map(function(d) { return d.name; }))
             .padding(0.1)
-            .rangeRound([10, width-10]);
+              .rangeRound([10, height-10]);
 
-    var y = d3.scaleLinear()
-            .domain([0, d3.max(dataset, function(d) {  return d3.max(d, function(d) { return d.y0 + d.y; });  })])
-            .range([height, 0]);
+    var x = d3.scaleLinear()
+            .domain([0, d3.max(dataset, function(series) {  return d3.max(series, function(d) { return d.end_ms; });  })])
+            .range([0, width]);
 
     let colors = d3.scaleOrdinal()
                    .domain(phases)
@@ -87,13 +87,13 @@ function render([phases, dataset]) {
 
     // Define and draw axes
     var yAxis = d3.axisLeft()
-                .scale(y)
-                .ticks(5)
-                .tickSize(-width, 0, 0)
-                .tickFormat( function(d) { return d } );
+                .scale(y);
 
     var xAxis = d3.axisBottom()
-                .scale(x);
+                .scale(x)
+                .ticks(5)
+                .tickSize(-width, 0, 0)
+                .tickFormat( function(d) { return d/1000 } );
 
     svg.append("g")
     .attr("class", "y axis")
@@ -102,14 +102,7 @@ function render([phases, dataset]) {
     svg.append("g")
        .attr("class", "x axis")
        .attr("transform", "translate(0," + height + ")")
-       .call(xAxis)
-       .selectAll("text")
-       .attr("y", 0)
-       .attr("x", 9)
-       .attr("dy", ".35em")
-       .attr("transform", "rotate(90)")
-       .style("text-anchor", "start");
-
+       .call(xAxis);
 
     // Create groups for each series, rects for each segment
     var groups = svg.selectAll("g.cost")
@@ -122,20 +115,20 @@ function render([phases, dataset]) {
                     .data(d => d)
                     .enter()
                     .append("rect")
-                    .attr("x", function(d) { return x(d.x); })
-                    .attr("y", function(d) { return y(d.y0 + d.y); })
-                     .attr("height", function(d) {
-                         let h = y(d.y0) - y(d.y0 + d.y)
+                    .attr("x", function(d) { return x(d.start_ms); })
+                    .attr("y", function(d) { return y(d.name); })
+                     .attr("width", function(d) {
+                         let h = x(d.end_ms) - x(d.start_ms)
                          if (isNaN(h)) { console.log(d); }
                          return h; })
-                    .attr("width", x.bandwidth() )
+                    .attr("height", y.bandwidth() )
                     .on("mouseout", function() { tooltip.style("display", "none"); })
                     .on("mousein", function() { tooltip.style("display", "block"); })
                     .on("mousemove", function(d) {
                         var xPosition = d3.mouse(this)[0] - 15;
                         var yPosition = d3.mouse(this)[1] - 25;
                         tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
-                        tooltip.select("text").text(d.y);
+                        tooltip.select("text").text(d.end_ms);
                     });
 
 
