4c7450df7fec278580d4667d75405fc554fe9e20
Daniel Bergey <bergey@simspace.com>
2020-01-23 17:33:11 -0500

    delete unused example data

--- stackedBar.js
+++ stackedBar.js
@@ -12,36 +12,6 @@ var svg = d3.select("body")
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-/* Data in strings like it would be if imported from a csv */
-
-var data = [
-    { year: "2006", redDelicious: "10", mcintosh: "15", oranges: "9", pears: "6" },
-    { year: "2007", redDelicious: "12", mcintosh: "18", oranges: "9", pears: "4" },
-    { year: "2008", redDelicious: "05", mcintosh: "20", oranges: "8", pears: "2" },
-    { year: "2009", redDelicious: "01", mcintosh: "15", oranges: "5", pears: "4" },
-    { year: "2010", redDelicious: "02", mcintosh: "10", oranges: "4", pears: "2" },
-    { year: "2011", redDelicious: "03", mcintosh: "12", oranges: "6", pears: "3" },
-    { year: "2012", redDelicious: "04", mcintosh: "15", oranges: "8", pears: "1" },
-    { year: "2013", redDelicious: "06", mcintosh: "11", oranges: "9", pears: "4" },
-    { year: "2014", redDelicious: "10", mcintosh: "13", oranges: "9", pears: "5" },
-    { year: "2015", redDelicious: "16", mcintosh: "19", oranges: "6", pears: "9" },
-    { year: "2016", redDelicious: "19", mcintosh: "17", oranges: "5", pears: "7" },
-];
-
-var parse = d3.timeParse("%Y");
-
-
-// Transpose the data into layers
-let stack = d3.stack()
-                .keys(["redDelicious", "mcintosh", "oranges", "pears"]);
-let dataset = stack(data)
-    .map(series =>
-        series.map( d => ({
-            x: d.data.year,
-            y0: d[0],
-            y: d[1]
-    })));
-
 var csv = d3.csv('ghc-passes-short.csv',
              d => {
                 d.milliseconds = +d.milliseconds;
